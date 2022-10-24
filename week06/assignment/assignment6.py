5e6881db5f7fb17637cbbc617bd5b742843fe6c0
JaxonAtBYUI <91147570+JaxonAtBYUI@users.noreply.github.com>
2022-10-24 15:22:43 -0600

    Completed Assignment 6

--- week06/assignment/assignment6.py
+++ week06/assignment/assignment6.py
@@ -2,10 +2,13 @@
 Course: CSE 251
 Lesson Week: 06
 File: assignment.py
-Author: <Your name here>
+Author: Jaxon Hamm
 Purpose: Processing Plant
 Instructions:
 - Implement the classes to allow gifts to be created.
+
+I believe that all requirements have been fullfilled as specified in the assignment.
+Because of this I feel that this assignment falls under category 4.
 """
 
 import random
@@ -16,6 +19,7 @@ import datetime
 
 # Include cse 251 common Python files - Don't change
 from cse251 import *
+from matplotlib import colors
 
 CONTROL_FILENAME = 'settings.txt'
 BOXES_FILENAME   = 'boxes.txt'
@@ -45,7 +49,6 @@ class Bag():
     def __str__(self):
         return str(self.items)
 
-
 class Gift():
     """ Gift of a large marble and a bag of marbles - Don't change """
 
@@ -57,7 +60,6 @@ class Gift():
         marbles = str(self.marbles)
         marbles = marbles.replace("'", "")
         return f'Large marble: {self.large_marble}, marbles: {marbles[1:-1]}'
-
 
 class Marble_Creator(mp.Process):
     """ This class "creates" marbles and sends them to the bagger """
@@ -81,9 +83,11 @@ class Marble_Creator(mp.Process):
         'Brown', 'Gold', 'Blue-Green', 'Antique Bronze', 'Mint Green', 'Royal Blue', 
         'Light Orange', 'Pastel Blue', 'Middle Green')
 
-    def __init__(self):
+    def __init__(self, connection, marble_count, delay):
         mp.Process.__init__(self)
-        # TODO Add any arguments and variables here
+        self.connection = connection
+        self.marble_count = marble_count
+        self.delay = delay
 
     def run(self):
         '''
@@ -93,15 +97,29 @@ class Marble_Creator(mp.Process):
             sleep the required amount
         Let the bagger know there are no more marbles
         '''
-        pass
+        
+        random.seed()
+        # Create random marble and send it away
+        for i in range(self.marble_count):
+            marble = self.colors[random.randint(0, len(self.colors) - 1)]
+            self.connection.send(marble)
+            time.sleep(self.delay)
 
+        
+        # Send end message and close the pipe.
+        self.connection.send(None)
+        self.connection.close()
 
 class Bagger(mp.Process):
     """ Receives marbles from the marble creator, then there are enough
         marbles, the bag of marbles are sent to the assembler """
-    def __init__(self):
+    def __init__(self, from_conn, to_conn, marble_count, bags, delay):
         mp.Process.__init__(self)
-        # TODO Add any arguments and variables here
+        self.from_conn = from_conn
+        self.to_conn = to_conn
+        self.bag_size = int(marble_count / bags) + (marble_count % bags > 0) # This is rounding up the number.
+        self.delay = delay
+
 
     def run(self):
         '''
@@ -112,17 +130,46 @@ class Bagger(mp.Process):
         tell the assembler that there are no more bags
         '''
 
+        # Our initial bag
+        bag = Bag()
+
+        while True:
+            # Get marble and see if we still need to run.
+            marble = self.from_conn.recv()
+            if marble == None:
+                break
+    
+            bag.add(marble)
+
+            # If the bag is full (or somehow more than full) send the bag through the pipe
+            # and get a new bag.
+            if bag.get_size() >= self.bag_size:
+                self.to_conn.send(bag)
+                bag = Bag()
+
+            # Its not totally clear where we are supposed to sleep here.
+            # Do we only sleep when we send something to the wrapper, or every loop?
+            time.sleep(self.delay)
+
+        # Send over final bag, let the wrapper know, and clean up.
+        self.from_conn.close()
+        self.to_conn.send(bag)
+        self.to_conn.send(None)
+        self.to_conn.close()
 
 class Assembler(mp.Process):
     """ Take the set of marbles and create a gift from them.
         Sends the completed gift to the wrapper """
     marble_names = ('Lucky', 'Spinner', 'Sure Shot', 'The Boss', 'Winner', '5-Star', 'Hercules', 'Apollo', 'Zeus')
 
-    def __init__(self):
+    def __init__(self, from_conn, to_conn, delay):
         mp.Process.__init__(self)
-        # TODO Add any arguments and variables here
+        self.from_conn = from_conn
+        self.to_conn = to_conn
+        self.delay = delay
 
     def run(self):
+        random.seed()
         '''
         while there are bags to process
             create a gift with a large marble (random from the name list) and the bag of marbles
@@ -130,13 +177,38 @@ class Assembler(mp.Process):
             sleep the required amount
         tell the wrapper that there are no more gifts
         '''
+        while True:
+            # Get bag from pipe and see if we are still running or not.
+            bag = self.from_conn.recv()
+            if bag == None:
+                break
 
+            # Create a large Marble
+            rand = random.randint(0, len(self.marble_names) - 1)
+            large_marble = self.marble_names[rand]
+
+            # Package into gift
+            gift = Gift(large_marble, bag)
+
+            # Send on pipe
+            self.to_conn.send(gift)
+
+            # Delay
+            time.sleep(self.delay)
+
+        # Clean up
+        self.from_conn.close()
+        self.to_conn.send(None)
+        self.to_conn.close()
 
 class Wrapper(mp.Process):
     """ Takes created gifts and wraps them by placing them in the boxes file """
-    def __init__(self):
+    def __init__(self, from_conn, filename, delay, count):
         mp.Process.__init__(self)
-        # TODO Add any arguments and variables here
+        self.from_conn = from_conn
+        self.filename = filename
+        self.delay = delay
+        self.count = count
 
     def run(self):
         '''
@@ -145,7 +217,18 @@ class Wrapper(mp.Process):
             save gift to the file with the current time
             sleep the required amount
         '''
+        with open(self.filename, 'w') as f:
+            
+            while True:
+                gift = self.from_conn.recv()
+                if gift == None:
+                    break
+                
+                self.count.value += 1
+                f.write(f'Created - {datetime.now().time()}: {gift}\n')
+                time.sleep(self.delay)
 
+            self.from_conn.close()
 
 def display_final_boxes(filename, log):
     """ Display the final boxes file to the log file -  Don't change """
@@ -156,8 +239,6 @@ def display_final_boxes(filename, log):
                 log.write(line.strip())
     else:
         log.write_error(f'The file {filename} doesn\'t exist.  No boxes were created.')
-
-
 
 def main():
     """ Main function """
@@ -180,8 +261,13 @@ def main():
     log.write(f'settings["wrapper-delay"]   = {settings[WRAPPER_DELAY]}')
 
     # TODO: create Pipes between creator -> bagger -> assembler -> wrapper
+    marble_in, marble_out = mp.Pipe()
+    bag_in, bag_out = mp.Pipe()
+    gift_in, gift_out = mp.Pipe()
 
     # TODO create variable to be used to count the number of gifts
+    # Do we not need the Value to keep the count?
+    count = mp.Value('i', 0)
 
     # delete final boxes file
     if os.path.exists(BOXES_FILENAME):
@@ -190,16 +276,26 @@ def main():
     log.write('Create the processes')
 
     # TODO Create the processes (ie., classes above)
+    processes = []
+    processes.append(Marble_Creator(marble_in, settings[MARBLE_COUNT], settings[CREATOR_DELAY]))
+    processes.append(Bagger(marble_out, bag_in, settings[MARBLE_COUNT], settings[BAG_COUNT], settings[BAGGER_DELAY]))
+    processes.append(Assembler(bag_out, gift_in, settings[ASSEMBLER_DELAY]))
+    processes.append(Wrapper(gift_out, BOXES_FILENAME, settings[WRAPPER_DELAY], count))
 
     log.write('Starting the processes')
     # TODO add code here
+    for p in processes:
+        p.start()
 
     log.write('Waiting for processes to finish')
-    # TODO add code here
+    # TODO add code 
+    for p in processes:
+        p.join()
 
     display_final_boxes(BOXES_FILENAME, log)
 
     # TODO Log the number of gifts created.
+    log.write(f'Total Gifts Created: {count.value}')
 
 
 
--- week06/team/team.py
+++ week06/team/team.py
@@ -17,12 +17,13 @@ After you can copy a text file word by word exactly
 
 import multiprocessing as mp
 from multiprocessing import Value, Process
-import filecmp 
+import filecmp
+import multiprocessing 
 
 # Include cse 251 common Python files
 from cse251 import *
 
-def sender():
+def sender(conn, filename):
     """ function to send messages to other end of pipe """
     '''
     open the file
@@ -30,17 +31,34 @@ def sender():
     Note: you must break each line in the file into words and
           send those words through the pipe
     '''
-    pass
+    with open(filename, 'rb') as f:
+        # Grab file line by line
+        for line in f:
+            # Grab line word by word
+            for word in line:
+                conn.send(word)
+
+        conn.send(None)
+        conn.close()
 
 
-def receiver():
+
+def receiver(conn, count, filename):
     """ function to print the messages received from other end of pipe """
     ''' 
     open the file for writing
     receive all content through the shared pipe and write to the file
     Keep track of the number of items sent over the pipe
     '''
-    pass
+    with open(filename, 'w') as f:
+        while True:
+            word = conn.recv()
+            if word == None:
+                break
+            
+            count.value += 1
+            f.write(word)
+        conn.close()
 
 
 def are_files_same(filename1, filename2):
@@ -50,22 +68,30 @@ def are_files_same(filename1, filename2):
 
 def copy_file(log, filename1, filename2):
     # TODO create a pipe 
-    
-    # TODO create variable to count items sent over the pipe
+    copy_conn, paste_conn = multiprocessing.Pipe()
 
-    # TODO create processes 
+    # TODO create variable to count items sent over the pipe
+    count = Value('i', 0)
+
+    # TODO create processes
+    copier = Process(target=sender, args=(copy_conn, filename1))
+    paster = Process(target=receiver, args=(paste_conn, count, filename2))
 
     log.start_timer()
     start_time = log.get_time()
 
-    # TODO start processes 
+    # TODO start processes
+    copier.start()
+    paster.start()
     
     # TODO wait for processes to finish
+    copier.join()
+    paster.join()
 
     stop_time = log.get_time()
 
-    log.stop_timer(f'Total time to transfer content = {PUT YOUR VARIABLE HERE}: ')
-    log.write(f'items / second = {PUT YOUR VARIABLE HERE / (stop_time - start_time)}')
+    log.stop_timer(f'Total time to transfer content = {count.value}: ')
+    log.write(f'items / second = {count.value / (stop_time - start_time)}')
 
     if are_files_same(filename1, filename2):
         log.write(f'{filename1} - Files are the same')
@@ -80,5 +106,5 @@ if __name__ == "__main__":
     copy_file(log, 'gettysburg.txt', 'gettysburg-copy.txt')
     
     # After you get the gettysburg.txt file working, uncomment this statement
-    # copy_file(log, 'bom.txt', 'bom-copy.txt')
+    copy_file(log, 'bom.txt', 'bom-copy.txt')
 
